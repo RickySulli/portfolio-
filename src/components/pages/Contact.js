a2340afa9c9a8bb4a3a052bf07e55a17ae0f90d2
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-04-02 00:47:57 -0500

    remove text

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,29 +11,31 @@ var style = {
     color:"beige",
     padding:"5%",
     overflow: "hidden"
+
 }
+
 const Contact = () => (
   <div className="row no-gutters" style={style}>
             {/* {Column1} */}
             <div className = "col"style={{display:"flex",justifyContent:"space-evenly",flexFlow:"column",alignItems:"center" }}>
                 
-                <h3 className="row">Give me a jingle</h3>
+                <h3 className="row" ></h3>
                 <h1><a href="tel:+1-817-690-1019"><i class="fas fa-phone"></i></a></h1>
             </div>
             {/* {Column2} */}
             <div className= "col"style={{display:"flex",justifyContent:"space-evenly",flexFlow:"column",alignItems:"center" }}>
-                <h3>Send me an Email</h3>
+                <h4></h4>
                 <h1><a href="mailto:dostuffforme@gmail.com?subject =Feedback@body =Message"><i class="far fa-envelope"></i></a></h1>
             </div>
             {/* {Column3} */}
             <div className= "col"style={{display:"flex",justifyContent:"space-evenly",flexFlow:"column",alignItems:"center" }}>
-              <h3>Check out my Repos</h3>
+              <h3></h3>
                 <h1>
                 <a href="https://github.com/rickysulli"><i class="fab fa-github"></i></a>
                 </h1>
             </div>
             <div className= "col"style={{display:"flex",justifyContent:"space-evenly",flexFlow:"column",alignItems:"center" }}>
-              <h3>Visit my Channel</h3>
+              <h3></h3>
                 <h1>
                 <a href="https://youtube.com/channel/UCaGkSXwQFxNjYYDPriCr-sw"><i class="fab fa-youtube"></i></a>
                 </h1>
